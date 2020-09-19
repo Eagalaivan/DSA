e4b581dd929be1098bbba36d73f71aecdd07a296
Eagalaivan <eagalaivanram@gmail.com>
2020-09-19 12:11:24 +0530

    commit

--- singlylinkedlist.py
+++ singlylinkedlist.py
@@ -35,7 +35,69 @@ class SinglyLinkedList:
                     print("found at pos",pos)
                 pos+=1
                 temp = temp.next
-            
+    def insertfirst(self,value):
+        temp=Node(value)
+        temp.next=self.head
+        self.head=temp
+    def insertlast(self,value):
+        temp=Node(value)
+        if self.head is None:
+            self.head = temp
+            return
+        else:
+            p=self.head
+            while p.next is not None:
+                p=p.next
+            p.next=temp
+    def insertafternode(self,value):
+        p=self.head
+        while p is not None:
+            if p.value == value:
+                break
+            p=p.next
+            if p is None:
+                print(value,"not found")
+            else:
+                temp=Node(value)
+                temp.next=p.next
+                p.next=temp
+    def insertbeforenode(self,value):
+        if self.head is None:
+            print("empty list")
+            return
+        if value == self.head.value:
+            temp=Node(value)
+            temp.next=self.head
+            self.head = temp
+            return
+        p=self.head
+        while p.link is not None:
+            if p.next.value == value:
+                break
+            p=p.next
+        if p.next is None:
+            print("value","not present in list")
+        else:
+            temp=Node(value)
+            temp.next=p.next
+            p.next=temp
+    def insertatposition(self,value,pos):
+        if pos==1:
+            temp=Node(value)
+            temp.next=self.head
+            self.head=temp
+            return
+        p=self.head
+        i=1
+        while i<pos-1 and p is not None:
+            p=p.next
+            i+=1
+        if p is None:
+            print("can only inset up postion",i)
+        else:
+            temp=Node(value)
+            temp.next=p.next
+            p.next=temp
 object= SinglyLinkedList()
 node1= Node(1)
 object.head=node1
@@ -49,3 +111,7 @@ node3.next=node4
 object.printNodes()
 object.countNodes()
 object.serachNode(3)
+object.insertfirst(0)
+object.insertlast(5)
+object.insertafternode(5)
+object.printNodes()
